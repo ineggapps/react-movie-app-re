a659485677c7fb783309bdffcf2a590e8b5d20ad
inegg <inegg.apps@gmail.com>
2019-05-13 20:21:43 +0900

    Styling CSS
    Styling CSS for responsive web pages that make available in all devices.

--- src/App.js
+++ src/App.js
@@ -6,46 +6,39 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Avengers",
-            poster: "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg"
-          },
-          {
-            title: "Matrix",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-          },
-          {
-            title: "Star wars",
-            poster:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY38LKiZ8gWEstGRDOvH0T0e8t7QgwChguucdmWeHQ9O2PWYkL"
-          }
-        ]
-      });
-    }, 2000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
     });
     return movies;
   };
 
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
   render() {
-    console.log("render");
     return <div className="App">{this.state.movies ? this._renderMovies() : "Loading..."}</div>;
   }
 }
