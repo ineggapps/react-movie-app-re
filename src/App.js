922d6c6e0a4a81604096e6bfc960c3274e8362cd
inegg <inegg.apps@gmail.com>
2019-05-13 13:23:36 +0900

    Loading states has been printed in web browser.
    Loading states has been printed in web browser.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,7 @@ import "./App.css";
 import Movie from "./Movie";
 
 class App extends Component {
-  state = {
-    movies: [
-      {
-        title: "Matrix",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-      },
-      {
-        title: "Star wars",
-        poster:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY38LKiZ8gWEstGRDOvH0T0e8t7QgwChguucdmWeHQ9O2PWYkL"
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     setTimeout(() => {
@@ -35,21 +13,40 @@ class App extends Component {
             title: "Avengers",
             poster: "https://upload.wikimedia.org/wikipedia/en/0/0d/Avengers_Endgame_poster.jpg"
           },
-          ...this.state.movies
+          {
+            title: "Matrix",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+          },
+          {
+            title: "Star wars",
+            poster:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY38LKiZ8gWEstGRDOvH0T0e8t7QgwChguucdmWeHQ9O2PWYkL"
+          }
         ]
       });
     }, 2000);
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     console.log("render");
-    return (
-      <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />;
-        })}
-      </div>
-    );
+    return <div className="App">{this.state.movies ? this._renderMovies() : "Loading..."}</div>;
   }
 }
 
